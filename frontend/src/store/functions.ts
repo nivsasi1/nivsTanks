e2aa8168d4182177c41cf8bf7cd0973df5e0068d
nivsasi1 <nivsasi@gmail.com>
2026-07-12 23:11:18 +0300

    chore: drop stale test comment

--- frontend/src/store/functions.ts
+++ frontend/src/store/functions.ts
@@ -3,7 +3,6 @@ import { tankNumber, loginInfo } from "./context-types";
 import { Errors } from "../assets/constants";
 
   export const loginTry = async (data: loginInfo) => {
-    //workin pernr for text :8604191, nonManager :8667298
       try{
         const response = await fetch("http://localhost:3000/login", {
         method: "POST",
